901524501c3d030ec8b7810229d21bd3a7895589
camcgibbon <connor13@illinois.edu>
2023-09-03 17:03:06 -0500

    hehehe

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 #SQLite Database
 app.secret_key = os.environ.get('SECRET_KEY')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI').replace('mysql://', 'mysql+pymysql://')
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
